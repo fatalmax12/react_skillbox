f9c57dc088000bc8db96a88f47c21439c0073c73
Maxim <m.shelenkov2017@yandex.ru>
2021-12-02 18:05:11 +0300

    -

---
+++ task_3/cfg/webpack.client.config.js
@@ -0,0 +1,46 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
+const IS_PROD = NODE_ENV === 'production';
+
+function setupDevtool() {
+  if (IS_PROD) {
+    return false;
+  }
+
+  if (IS_DEV) {
+    return 'eval';
+  }
+}
+
+module.exports = {
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: './src/client/index.tsx',
+  output: {
+    filename: 'client.js',
+    path: path.resolve(__dirname, '../dist/client'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: 'src/index.html'
+  //   })
+  // ],
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".json", ".jsx"]
+  },
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  },
+  devtool: setupDevtool(),
+};
---
+++ task_3/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+//const ReactDOM = require('react-dom');
+//import { Header } from './src/Header';
+//const App = require('./src/App');
+import { App } from './src/App';
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
